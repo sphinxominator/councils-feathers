5b3d48e19c419e414ed60d343c161bef9b297deb
Viktor Shamal Andersen <sphinxominator@gmail.com>
2017-05-21 13:11:48 +0200

    Added an Auth provider

--- app/src/App.js
+++ app/src/App.js
@@ -2,13 +2,10 @@ import React from 'react';
 
 import { ApolloClient,  ApolloProvider, createNetworkInterface } from 'react-apollo';
 import styled, { keyframes } from 'styled-components';
-import Auth0Lock from 'auth0-lock';
-import feathers from 'feathers-client';
-import socketio from 'feathers-socketio/client'
-import io from 'socket.io-client';
 
 import logo from './logo.svg';
 import { MeetingsForm, MeetingsList } from './components/Meetings';
+import { AuthProvider, LoginButton } from './components/Auth';
 
 const networkInterface = createNetworkInterface({
   opts: {
@@ -19,60 +16,21 @@ const networkInterface = createNetworkInterface({
 
 const client = new ApolloClient({ networkInterface });
 
-const lock = new Auth0Lock('B1Y0w8pMQ9LXK5REYBigK06PGvzqdCK0', 'councils.eu.auth0.com', {
-  auth: {
-    redirectUrl: 'http://localhost:3000/auth/callback',
-    responseType: 'code',
-    params: {
-      scope: 'openid email'
-    }
-  }
-});
-
-export default class App extends React.Component {
-  state = { meetings: null }
-
-  constructor() {
-    super()
-    //this.mountSockets();
-    this.lock = lock;
-  }
-
-  showLock() {
-    this.lock.show();
-  }
-
-  mountSockets() {
-    const socket = io('http://localhost:3001');
-    const app = feathers();
-    app.configure(socketio(socket));
-
-    const meetingService = app.service('meetings');
-
-    meetingService.on('created', (meeting) => {
-      this.setState({ meetings: this.state.meetings.concat([meeting]) });
-    })
-  }
-
-  render() {
-    return (
-      <ApolloProvider client={client}>
-        <Main>
-          <Header>
-            <Logo src={logo} alt="logo" />
-            <h2>Welcome to React</h2>
-          </Header>
-          <Intro onClick={()=>this.showLock()}>
-            To get started, edt <code>src/App.js</code> and save to reload
-          </Intro>
-          <MeetingsForm />
-          <MeetingsList />
-
-        </Main>
-      </ApolloProvider>
-    );
-  }
-}
+export default () => (
+  <ApolloProvider client={client}>
+    <AuthProvider>
+      <Main>
+        <Header>
+          <Logo src={logo} alt="logo" />
+        <h2>Welcome to Councils</h2>
+        </Header>
+        <LoginButton />
+        <MeetingsForm />
+        <MeetingsList />
+      </Main>
+    </AuthProvider>
+  </ApolloProvider>
+);
 
 const rotate360 = keyframes`
   from {
@@ -98,8 +56,4 @@ const Header = styled.div`
   height: 150px;
   padding: 20px;
   color: white;
-`
-
-const Intro = styled.p`
-  font-size: large;
 `
--- app/src/components/Auth.js
+++ app/src/components/Auth.js
@@ -1,11 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
 import Auth0Lock from 'auth0-lock';
 
-const lock = new Auth0Lock('B1Y0w8pMQ9LXK5REYBigK06PGvzqdCK0', 'councils.eu.auth0.com', {
-  auth: {
-    redirectUrl: 'http://localhost:3000/auth/callback',
-    responseType: 'code',
-    params: {
-      scope: 'openid email'
-    }
+export class AuthProvider extends React.Component {
+  constructor(props) {
+    super(props);
+    this.lock = new Auth0Lock('B1Y0w8pMQ9LXK5REYBigK06PGvzqdCK0', 'councils.eu.auth0.com', {
+      auth: {
+        redirectUrl: 'http://localhost:3000/auth/callback',
+        responseType: 'code',
+        params: {
+          scope: 'openid email'
+        }
+      }
+    });
   }
-});
+
+  getChildContext() {
+    return { lock: this.lock }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+AuthProvider.childContextTypes = {
+  lock: PropTypes.object
+}
+
+export const LoginButton = ({ label = 'Login' }, { lock }) => (
+  <Button onClick={() => lock.show() }> { label } </Button>
+)
+
+LoginButton.contextTypes = {
+  lock: PropTypes.object
+}
+
+const Button = styled.button`
+  background-color: blue;
+  color: white;
+`
