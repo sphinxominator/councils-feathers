5ce19ed352d7ffbe799dd1b15d98271a78139b0e
Viktor Shamal Andersen <sphinxominator@gmail.com>
2017-06-07 15:35:43 +0200

    Trying to fix callback mismatch

--- src/services/authentication/authentication.service.js
+++ src/services/authentication/authentication.service.js
@@ -23,7 +23,7 @@ module.exports = function () {
       domain: process.env.AUTH0_DOMAIN,
       clientID: process.env.AUTH0_CLIENT_ID,
       clientSecret: process.env.AUTH0_CLIENT_SECRET,
-      callbackURL: '/auth/callback'
+      callbackURL: process.env.URI + '/auth/callback'
     },
     function(accessToken, refreshToken, extraParams, profile, done) {
       return done(null, { profile, token: extraParams.id_token });
