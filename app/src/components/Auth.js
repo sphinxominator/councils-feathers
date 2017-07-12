5ce941d5ab4f3dc3f98211ef422536ddf19be2ec
Viktor Shamal Andersen <sphinxominator@gmail.com>
2017-07-12 18:04:02 +0200

    Login button and head links

--- app/src/components/Auth.js
+++ app/src/components/Auth.js
@@ -2,15 +2,15 @@ import React from 'react'
 import styled from 'styled-components'
 
 import { connect } from 'react-redux'
-import { withHandlers, compose, pure } from 'recompose'
+import { withHandlers, withProps, compose, pure } from 'recompose'
 
-const LoginButtonPure = ({ onClick, auth }) =>
-  <Button onClick={onClick} logout={!!auth}>
-    {' '}{!!auth ? 'Log ud' : 'Log ind'}{' '}
+const LoginButtonPure = ({ onClick, isLogin = true }) =>
+  <Button onClick={onClick}>
+    {' '}{isLogin ? 'Log ind' : 'Log ud'}{' '}
   </Button>
 
 const Button = styled.button`
-  background-color: ${props => (props.logout ? 'purple' : 'blue')};
+  background-color: ${props => (props.isLogin ? 'blue' : 'purple')};
   border: 1px solid black;
   color: white;
   height: 1.5rem;
@@ -31,5 +31,6 @@ export const LoginButton = compose(
       !props.auth ? authorize(props.lock) : console.log('loging out')
     }
   }),
+  withProps(props => ({ isLogin: !props.auth })),
   pure
 )(LoginButtonPure)
