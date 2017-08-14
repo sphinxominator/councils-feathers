e844e13ce681d3013428851ef0964b407de1114e
Viktor Shamal Andersen <sphinxominator@gmail.com>
2017-08-14 18:06:04 +0200

    Let's try to serve files differently

--- src/server.js
+++ src/server.js
@@ -36,7 +36,7 @@ app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use(express.static(process.env.RAZZLE_PUBLIC_DIR))
+app.use(express.static('../build/public'))
 //app.use(express.static('public'))
 
 // Set up Plugins and providers .
