c2dcaf2f28046219177999f4a4d03695fce3a0d2
Viktor Shamal Andersen <sphinxominator@gmail.com>
2017-09-19 20:20:50 +0200

    auth0 service

--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,5 @@
 const authentication = require('./authentication/authentication.service.js')
+const auth0 = require('./auth0/auth0.service.js')
 const graphql = require('./graphql/graphql.service.js')
 const meetings = require('./meetings/meetings.service.js')
 const users = require('./users/users.service.js')
@@ -12,5 +13,6 @@ module.exports = function() {
   app.configure(users)
   app.configure(groups)
   app.configure(attendants)
+  app.configure(auth0)
   app.configure(graphql)
 }
